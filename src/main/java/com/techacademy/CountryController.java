daad48be8a9526b2459430d3ccc22ded8c8dc716
waukan <uni@waukan.sakura.ne.jp>
2024-07-23 01:01:23 +0900

    Lesson16課題です。

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -48,8 +48,8 @@ public class CountryController {
     }
 
     // -----削除画面-----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = {"/delete", "/delete/{code}/"})
+    public String deleteCountryForm(@PathVariable(name = "code", required = false)String code, Model model) {
         // country/delete.htmlに画面遷移
         return "country/delete";
     }
